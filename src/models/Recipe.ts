4dfa39a764ef1fc675ba0e7e4e9d2c9ff4e195f9
Ellinor Johansson <ellinor.asaka@gmail.com>
2026-05-13 11:10:18 +0200

    🗃️(db): added more to recipe model

--- src/models/Recipe.ts
+++ src/models/Recipe.ts
@@ -1,19 +1,45 @@
-import mongoose, { Schema, Model } from 'mongoose';
+import mongoose, { Schema, Model } from "mongoose";
 
 export interface IRecipe {
-	_id?: string;
-	name: string;
+  _id?: string;
+  name: string;
+  description: string;
+  tag: string;
+  cookingTime: string;
+  ingredients: string;
+  link: string;
+  prepTime: string;
+  instructions: string;
 }
 
-const RecipeSchema = new Schema<IRecipe>(
-	{
-		name: {
-			type: String,
-			required: true,
-		}
-	}
-);
+const RecipeSchema = new Schema<IRecipe>({
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  tag: {
+    type: String,
+  },
+  cookingTime: {
+    type: String,
+  },
+  ingredients: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  prepTime: {
+    type: String,
+  },
+  instructions: {
+    type: String,
+  },
+});
 
-const Race: Model<IRecipe> = mongoose.models.Race || mongoose.model<IRecipe>('Recipe', RecipeSchema);
+const Race: Model<IRecipe> =
+  mongoose.models.Race || mongoose.model<IRecipe>("Recipe", RecipeSchema);
 
 export default Race;
